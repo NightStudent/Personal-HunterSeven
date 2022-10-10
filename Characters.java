7084d223444363f2a94633a5332f02906465e467
NightStudent <105054834+NightStudent@users.noreply.github.com>
2022-10-10 14:30:21 +0200

    Update Characters.java

--- Characters.java
+++ Characters.java
@@ -4,9 +4,6 @@ public interface Characters {
 	int health();
 	int mana();
 	
-	//void move (); 
-    //void strike(int damageMin, int damageMax); 
-	
-	//meant to contain all the varibles and methods the player and monsters will all have 
+//players and monsters are meant to be able to cast spells and have health
 	
 }
