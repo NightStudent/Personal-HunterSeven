ed46c7194db3b62111e1aeb9a3f5521334904cb9
NightStudent <105054834+NightStudent@users.noreply.github.com>
2022-10-10 14:27:41 +0200

    Update Characters.java

--- Characters.java
+++ Characters.java
@@ -7,5 +7,6 @@ public interface Characters {
 	//void move (); 
     //void strike(int damageMin, int damageMax); 
 	
+	//meant to contain all the varibles and methods the player and monsters will all have 
 	
 }
