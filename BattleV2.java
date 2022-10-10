4ddecd53c2c2748561218c221365953b89caf984
NightStudent <105054834+NightStudent@users.noreply.github.com>
2022-10-10 14:23:20 +0200

    Add files via upload

---
+++ BattleV2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class BattleV2 {
+	
+	public class battleV2 extends fighter{
+	
+		 public static void display() {
+		 
+	    System.out.println(" actions points is: " + actionPoints);
+	    System.out.println(" hp is: " + health);
+	    System.out.println(" mana is: " + mana);
+
+	    
+		 return;
+		 }
+		
+
+	public static void main(String[] args) {
+		
+		actionPoints = 2;
+		health = 100;
+		mana = 200;
+		
+		display();
+	
+		
+		
+		
+}
+ }
+  }
